321dc5ad41e3eb45d497521d82fc5ec75c2ae960
Jorge L <jorge.lopez@newfold.com>
2023-12-04 19:11:37 -0400

    create DbContext

---
+++ Data/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jal_crud.Data
+{
+    public class Context: DbContext
+    {
+        public Context()
+        {
+            if (DeviceInfo.Platform == DevicePlatform.iOS || DeviceInfo.Platform == DevicePlatform.MacCatalyst)
+            {
+                SQLitePCL.Batteries_V2.Init();
+            }
+
+            this.Database.EnsureCreated();
+            this.Database.Migrate();
+        }
+    }
+}
