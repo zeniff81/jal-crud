eed29d70af72c5a0c12e1aa5cc21cb711b6aa8f5
Jorge L <jorge.lopez@newfold.com>
2023-12-04 18:48:36 -0400

    Start creating contactos model

---
+++ Models/clsContactosBE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jal_crud.Models
+{
+    [Table("Contactos")]
+    public class clsContactosBE
+    {
+        public int ContactosId { get; set; }
+        public DateTime Fecha { get; set; }
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+
+        public clsContactosBE()
+        {
+            
+        }
+    }    
+}
