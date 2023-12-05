920dff0dfc2859269790c4c28a453fc1647c3692
Jorge L <jorge.lopez@newfold.com>
2023-12-04 20:26:56 -0400

    Add annotations to clsContactosBE

--- Models/clsContactosBE.cs
+++ Models/clsContactosBE.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,11 @@ namespace jal_crud.Models
     [Table("Contactos")]
     public class clsContactosBE
     {
+        [Key]
         public int ContactosId { get; set; }
         public DateTime Fecha { get; set; }
+        
+        [MaxLength(100)]
         public String Nombres { get; set; }
         public String Apellidos { get; set; }
         public String Direccion { get; set; }
