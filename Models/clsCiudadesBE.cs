97e267da94e311cc2533e3f4460ff57ac656191f
Jorge L <jorge.lopez@newfold.com>
2023-12-06 11:39:39 -0400

    Create ciudades model

---
+++ Models/clsCiudadesBE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jal_crud.Models
+{
+    public class clsCiudadesBE
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CiudadId { get; set; }
+        
+        [MaxLength(50)]
+        public string Ciudad { get; set; }
+
+        // relaciones 
+        public virtual ICollection<clsContactosBE> Contactos { get; set; }
+    }
+}
